24756b9543b5c48d261c3d320eb2c8e4fa392eec
Gyun-Gi-Min <mingg95@gmail.com>
2022-01-04 16:42:12 +0900

    community spring

---
+++ src/main/java/com/koreait/community/UserUtils.java
@@ -0,0 +1,6 @@
+package com.koreait.community;
+
+public class UserUtils {
+    //로그인한 사람 PK 가져오는거
+
+}
---
+++ src/main/java/com/koreait/community/user/UserController.java
@@ -0,0 +1,18 @@
+package com.koreait.community.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/join")
+    public void join(){ //WEB-INF/views/user/join.jsp로 간다
+    }
+
+
+
+
+}
